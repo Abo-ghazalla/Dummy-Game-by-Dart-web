07731b938c56195c18747486853bb3de28087f3a
Mohamed Abo-ghazalla <45886535+Abo-ghazalla@users.noreply.github.com>
2020-09-03 15:05:55 +0200

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,31 @@
+import 'dart:html';
+
+void main() {
+  var header = querySelector('#header');
+  header.text = "Hello, World!";
+  var message = querySelector('#message');
+  
+  var begin = true;
+  
+  final ButtonElement button = querySelector("button");
+  button.onClick
+      .timeout(
+        Duration(seconds: 2),
+        onTimeout: (sink) {
+          sink.addError("Too Slow!! you lost.");
+          },
+      )
+      .listen(
+        (event) {
+          message.text = null;
+        },
+        onError: (e) {
+          if(begin){
+            begin = false;
+            return;
+          }
+          print(e);
+          message.text = e;
+        },
+      );
+}
